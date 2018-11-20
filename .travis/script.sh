bb1d26f7bb46050bc02c42624a9a7b5fe765ff90
Matheus Canon <knonm@disroot.org>
2018-11-20 13:41:18 -0200

    Changing deploy and build settings

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_EVENT_TYPE}" == push ] &&
-    echo "${TRAVIS_TAG}" | egrep '^[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?$'
-then
-    # the build is triggered by a tag push, and the tag looks like
-    # a version number: proceed with release
-    echo ${GPG_KEYS} | base64 --decode | gpg2 --import
-    mvn versions:set -DnewVersion=${TRAVIS_TAG}
-    mvn -s .travis/settings.xml -Prelease deploy
-else
-    # this is a regular build
-    mvn install
-fi
+# the build is triggered by a tag push, and the tag looks like
+# a version number: proceed with release
+gpg --keyserver hkp://keys.gnupg.net --receive-keys ${GPG_KEYNAME}
+mvn versions:set -DnewVersion=${TRAVIS_TAG}
+mvn -s .travis/settings.xml release -P deploy
