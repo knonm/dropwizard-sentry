7e27c7a9388beca08357977bc3405f90cb8d1a65
Matheus Canon <knonm@disroot.org>
2018-11-20 14:08:27 -0200

    Changing gpg parameter

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,6 +2,6 @@
 
 # the build is triggered by a tag push, and the tag looks like
 # a version number: proceed with release
-gpg --keyserver hkp://keys.gnupg.net --receive-keys ${GPG_KEYNAME}
+gpg --keyserver hkp://keys.gnupg.net --recv-keys ${GPG_KEYNAME}
 mvn versions:set -DnewVersion=${TRAVIS_TAG}
 mvn -s .travis/settings.xml release -P deploy
