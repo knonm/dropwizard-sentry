ed36bc9d6a1e2d5e2e4d53386910a4a9e3fd989d
Matheus Canon <knonm@disroot.org>
2018-11-20 17:33:13 -0200

    Fixing build when pushing to master

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+gpg --keyserver hkp://keys.gnupg.net --recv-keys ${GPG_KEYNAME}
+
 if [ "${TRAVIS_EVENT_TYPE}" == push ] &&
     echo "${TRAVIS_TAG}" | egrep '^[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?$'
 then
     # the build is triggered by a tag push, and the tag looks like
     # a version number: proceed with release
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys ${GPG_KEYNAME}
     mvn versions:set -DnewVersion=${TRAVIS_TAG}
     mvn -s .travis/settings.xml -Prelease deploy
 else
